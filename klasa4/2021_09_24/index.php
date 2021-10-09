656c46177d74b1b3975b4c390f9dfe9ec258a984
MBernart <mateuszbernart@gmail.com>
2021-10-09 16:55:42 +0200

    2021_09_24

---
+++ klasa4/2021_09_24/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl3.css">
+    <title>Filmoteka</title>
+</head>
+
+<body>
+    <header>
+        <div class="header-element">
+            <img src="klaps.png" alt="Nasze filmy">
+        </div>
+        <div class="header-element">
+            <h1>BAZA FILMÓW</h1>
+        </div>
+        <div class="header-element">
+            <form action="" method="post">
+                <select name="gatunek">
+                    <option value="Sci-Fi">Sci-Fi</option>
+                    <option value="animacja">animacja</option>
+                    <option value="dramat">dramat</option>
+                    <option value="horror">horror</option>
+                    <option value="komedia">komedia</option>
+                </select>
+                <input type="submit" value="Filmy">
+            </form>
+        </div>
+        <div class="header-element"><img src="gwiezdneWojny.jpg" alt="szturmowcy"></div>
+    </header>
+    <main>
+        <div class="main-element">
+            <h2>Wybrano filmy:</h2>
+            <p> PHP Skrypt nr 1 \PHP </p>
+        </div>
+        <div class="main-element">
+            <h2>Wszystkie filmy</h2>
+            <p> PHP Skrypt nr 2 \PHP </p>
+        </div>
+    </main>
+    <footer>
+        <p> Autor: Mateo </p>
+        <a href="">Zapytania do bazy</a>
+        <a href="filmy.pl" target="_blank">Przejdź do filmy.pl</a>
+    </footer>
+</body>
+
+</html>
