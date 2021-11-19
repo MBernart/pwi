5a44af9d4a1811a1c1940c856cbfa664f3527939
MBernart <mateuszbernart@gmail.com>
2021-11-19 13:33:44 +0100

    2021_11_19

--- klasa4/2021_09_24/index.php
+++ klasa4/2021_09_24/index.php
@@ -10,6 +10,9 @@
 </head>
 
 <body>
+    <?php
+        $connection = new mysqli('localhost', 'root', '', 'dane');
+    ?>
     <header>
         <div class="header-element">
             <img src="klaps.png" alt="Nasze filmy">
@@ -34,16 +37,35 @@
     <main>
         <div class="main-element">
             <h2>Wybrano filmy:</h2>
-            <p> PHP Skrypt nr 1 \PHP </p>
+            <?php
+                $sql = "SELECT `tytul`, `rok`, `ocena` 
+                        FROM 
+                            `filmy` 
+                        JOIN `gatunki` 
+                            ON `filmy`.`gatunki_id` = `gatunki`.`id` 
+                        WHERE `gatunki`.`nazwa` = 'komedia'";
+                $result = $connection->query($sql);
+                foreach ($result as $row) {
+                    echo "<p>Tytuł: $row[tytul], Rok produkcji: $row[rok], Ocena: $row[ocena]</p>";
+                }
+            ?>
         </div>
         <div class="main-element">
             <h2>Wszystkie filmy</h2>
-            <p> PHP Skrypt nr 2 \PHP </p>
+            
+            <?php
+                $sql = "SELECT `filmy`.`id`, `tytul`, `rezyserzy`.`imie`, `rezyserzy`.`nazwisko` FROM `filmy` JOIN `rezyserzy` ON `filmy`.`rezyserzy_id` = `rezyserzy`.`id`";
+                $result = $connection->query($sql);
+                foreach ($result as $row) {
+                    echo "<p>$row[id]. $row[tytul], reżyseria: $row[imie] $row[nazwisko]</p>";
+                }
+            ?>
+
         </div>
     </main>
     <footer>
         <p> Autor: Mateo </p>
-        <a href="">Zapytania do bazy</a>
+        <a href="kwerendy.txt">Zapytania do bazy</a>
         <a href="filmy.pl" target="_blank">Przejdź do filmy.pl</a>
     </footer>
 </body>
